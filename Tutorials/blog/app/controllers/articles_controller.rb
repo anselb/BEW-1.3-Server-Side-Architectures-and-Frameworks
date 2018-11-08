2dac30d54d50248803d9ccfb8e5cae5ff8f9ba12
Ansel Bridgewater <iamansel@gmail.com>
2018-11-07 17:43:52 -0800

    Added model validation and error messages for new articles

--- Tutorials/blog/app/controllers/articles_controller.rb
+++ Tutorials/blog/app/controllers/articles_controller.rb
@@ -2,12 +2,13 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
-  
+
   def show
     @article = Article.find(params[:id])
   end
 
   def new
+    @article = Article.new
   end
 
   def create
@@ -15,8 +16,11 @@ class ArticlesController < ApplicationController
     # render plain: params[:article].inspect
     @article = Article.new(article_params)
 
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
   private
