1bc325d756c840af2553d42fbd04644715d60be3
Ansel Bridgewater <iamansel@gmail.com>
2018-12-01 19:40:20 -0800

    Wrote a couple basci tests

--- Tutorials/integration-testing-minitest/test/integration/post_flow_test.rb
+++ Tutorials/integration-testing-minitest/test/integration/post_flow_test.rb
@@ -12,4 +12,19 @@ class PostFlowTest < ActionDispatch::IntegrationTest
     assert page.has_content?(@one.title)
     assert page.has_content?(@two.title)
   end
+
+  test 'writing a new post' do
+    visit posts_path
+
+    click_on 'New Post'
+
+    fill_in 'Title', with: 'Test Title'
+    fill_in 'Body',  with: 'Test Body'
+
+    click_on 'Create Post'
+
+    assert_current_path post_path(Post.last)
+    assert page.has_content?('Test Title')
+    assert page.has_content?('Test Body')
+  end
 end
